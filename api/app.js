4447548aecd456a693c9457f08ab447f899a9a58
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2018-08-16 17:43:39 +0000

    Added vue config

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 // app.js
 var express = require('express');
 var app = express();
+var proxy = require('http-proxy-middleware');
 
 // routes
 var testController = require('./testController')
 app.use('/', testController);
 
+// proxy
+app.use('/api', proxy({target: 'http://localhost:8080', changeOrigin: true}));
 
 module.exports = app;
