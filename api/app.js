22e01cc05fc8836921259e8481ff1f15d91070f3
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2018-08-17 13:45:38 +0000

    cleaned up some comments

--- api/app.js
+++ api/app.js
@@ -1,13 +1,9 @@
 // app.js
 var express = require('express');
 var app = express();
-var proxy = require('http-proxy-middleware');
 
 // routes
 var testController = require('./testController')
-app.use('/', testController);
-
-// proxy
-app.use('/api', proxy({target: 'http://localhost:8080', changeOrigin: true}));
+app.use('/api', testController);
 
 module.exports = app;
